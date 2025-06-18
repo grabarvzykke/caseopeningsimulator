d727b83be1d4f04809e639dce400e960aa62127b
grabarvzykke <grabaashop@gmail.com>
2025-06-18 23:45:28 +0200

    Update caseopeningsimulator.lua

--- caseopeningsimulator.lua
+++ caseopeningsimulator.lua
@@ -1,62 +1,47 @@
 local Rayfield = loadstring(game:HttpGet('https://[Log in to view URL]'))()
 
 local Window = Rayfield:CreateWindow({
-   Name = "all gamepasses script",
-   LoadingTitle = "all gamepasses script",
+   Name = "All Gamepasses Script",
+   LoadingTitle = "All Gamepasses Script",
    LoadingSubtitle = "by Sture_14",
    ConfigurationSaving = {
       Enabled = true,
-      FolderName = true, -- Create a custom folder for your hub/game
+      FolderName = true,
       FileName = "AGS"
-   },
-   Discord = {
-      Enabled = false,
-      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-   },
-   KeySystem = false, -- Set this to true to use our key system
-   KeySettings = {
-      Title = "Untitled",
-      Subtitle = "Key System",
-      Note = "No method of obtaining the key is provided",
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
 })
 
-local MainTab = Window:CreateTab("Main", 4483362458) -- Title, Image
-local Section = MainTab:CreateSection("main")
+local MainTab = Window:CreateTab("Main", 4483362458)
+local Section = MainTab:CreateSection("Main Features")
 
-local Button = MainTab:CreateButton({
-   Name = "Universal script",
+MainTab:CreateButton({
+   Name = "Universal Script",
    Callback = function()
-        loadstring(game:HttpGet("https://[Log in to view URL]"))() 
+        loadstring(game:HttpGet("https://[Log in to view URL]"))()
    end,
 })
 
-local Slider = MainTab:CreateSlider({
+MainTab:CreateSlider({
    Name = "WalkSpeed",
    Range = {0, 460},
    Increment = 1,
-   Suffix = "Speeeed",
+   Suffix = "Speed",
    CurrentValue = 16,
-   Flag = "Slider1", -- A flag is the identifier for the configuration file, make sure every element has a different flag if you're using configuration saving to ensure no overlaps
+   Flag = "Slider1",
    Callback = function(Value)
-        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = (Value)
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = Value
    end,
 })
 
-local Button = MainTab:CreateButton({
-   Name = "get all gamepasses",
+MainTab:CreateButton({
+   Name = "Get All Gamepasses",
    Callback = function()
         for _,v in pairs(game:GetDescendants()) do
-if v.ClassName == "RemoteEvent" then
-if v.Parent.Name == "WeaponsRemotes" or v.Parent.Name == "VipRemotes" or v.Parent.Name == "Remotes" then
-v:FireServer()
-end
-end
-end
+            if v.ClassName == "RemoteEvent" then
+                if v.Parent.Name == "WeaponsRemotes" or v.Parent.Name == "VipRemotes" or v.Parent.Name == "Remotes" then
+                    v:FireServer()
+                end
+            end
+        end
    end,
 })
